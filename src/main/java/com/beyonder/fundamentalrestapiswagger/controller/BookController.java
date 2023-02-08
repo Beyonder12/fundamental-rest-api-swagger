9b13a6d57ce563b47bc57bfd8eca57e55b40d7ab
Beyonder12 <fajri.illahi1211@gmail.com>
2023-02-08 20:06:32 +0700

    second endpoint

--- src/main/java/com/beyonder/fundamentalrestapiswagger/controller/BookController.java
+++ src/main/java/com/beyonder/fundamentalrestapiswagger/controller/BookController.java
@@ -1,9 +1,9 @@
 package com.beyonder.fundamentalrestapiswagger.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/books")
@@ -12,6 +12,16 @@ public class BookController {
     @GetMapping
     public String sayHello() {
         return "Hello!";
+    }
+
+    @GetMapping("/detail")
+    public Map<String, String> getBook(@RequestParam int id) {
+        Map<String, String> response = new HashMap<>();
+        response.put("code", "200");
+        response.put("message", "Horray get a data with id" + id);
+        response.put("status", "Success");
+        response.put("data", id + "");
+        return response;
     }
 
 }
