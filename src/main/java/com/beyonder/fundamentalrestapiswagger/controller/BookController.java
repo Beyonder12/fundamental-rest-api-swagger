1f211fef189d1547c43fbedc775116f6a4074967
Beyonder12 <fajri.illahi1211@gmail.com>
2023-02-08 20:27:18 +0700

    third enpoint

--- src/main/java/com/beyonder/fundamentalrestapiswagger/controller/BookController.java
+++ src/main/java/com/beyonder/fundamentalrestapiswagger/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.beyonder.fundamentalrestapiswagger.controller;
 
+import com.beyonder.fundamentalrestapiswagger.dto.BookDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -9,19 +12,29 @@ import java.util.Map;
 @RequestMapping("api/v1/books")
 public class BookController {
 
-    @GetMapping
+    @GetMapping("/")
     public String sayHello() {
         return "Hello!";
     }
 
     @GetMapping("/detail")
-    public Map<String, String> getBook(@RequestParam int id) {
+    public Map<String, String> getBook(@RequestParam int id, int name) {
         Map<String, String> response = new HashMap<>();
         response.put("code", "200");
-        response.put("message", "Horray get a data with id" + id);
+        response.put("message", "Horray get a data with id " + id + name);
         response.put("status", "Success");
         response.put("data", id + "");
         return response;
     }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<BookDTO> getBook(@PathVariable int id)  {
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setName("Physics " + id);
+        return new ResponseEntity<>(bookDTO, HttpStatus.OK);
+    }
+
+
+
 
 }
---
+++ src/main/java/com/beyonder/fundamentalrestapiswagger/dto/BookDTO.java
@@ -0,0 +1,14 @@
+package com.beyonder.fundamentalrestapiswagger.dto;
+
+
+public class BookDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
